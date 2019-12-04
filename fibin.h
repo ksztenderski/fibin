e8fe2541b3876d24c8625deea1b11bcce60a4ab1
Krystian <56883920+ksztenderski@users.noreply.github.com>
2019-12-04 13:11:58 +0100

    Update fibin.h
    Prawie koniec, TODO: Lambda, Invoke

--- fibin.h
+++ fibin.h
@@ -4,12 +4,11 @@
 #include <iostream>
 #include <cstdint>
 #include <typeinfo>
+#include <cassert>
 
 #define BASE 37
 
 using var_t = unsigned;
-
-
 
 template<int N>
 struct Fib {
@@ -22,11 +21,12 @@ struct False {
 };
 
 template<typename T>
-struct Lit {};
+struct Lit {
+};
 
 template<typename T1, typename T2, typename... Rest>
-struct Sum {};
-
+struct Sum {
+};
 
 template<typename Arg>
 struct Inc1 {
@@ -39,38 +39,26 @@ struct Inc10 {
 template<typename Left, typename Right>
 struct Eq {
 };
-//rzeczy do refa
 
-struct EmptyEnv;
-
-
-template<var_t Name, typename Value, typename Env>
-struct List {
+template<var_t Var>
+struct Ref {
 };
 
-template<var_t Name, typename Env>
-struct Find {
+template<typename Condition, typename Then, typename Else>
+struct If {
 };
 
-template<var_t Name>
-struct Find<Name, EmptyEnv> {
+template<var_t Var, typename Body>
+struct Lambda {
 };
 
-template<var_t Name, typename Value, typename Env>
-struct Find<Name, List<Name, Value, Env> > {
-    Value typedef result;
+template<typename Fun, typename Param>
+struct Invoke {
 };
 
-template<var_t Name, var_t Name2, typename Value2, typename Env>
-struct Find<Name, List<Name2, Value2, Env> > {
-    typename Find<Name, Env>::result typedef result;
-};
-
-template<var_t Name, typename Value, typename Env>
+template<var_t Name, typename Value, typename Expression>
 struct Let {
-    using result = List<Name, Value, Env>;
 };
-
 
 constexpr bool properVar(const char *str) {
     if (str[0] == '\0') return false;
@@ -84,15 +72,10 @@ constexpr bool properVar(const char *str) {
     return str[i] == '\0';
 }
 
-// constexpr to calculate the length of a string
-constexpr size_t str_len(const char *p_str) {
-    return (*p_str == '\0') ? 0 : str_len(p_str + 1) + 1;
-}
-
 constexpr var_t Var(const char *str) {
+    assert(properVar(str));
     int i = 0;
     var_t res = 0;
-    if (str[0] == '\0') return 0;
     while (i < 6 && str[i] != '\0') {
         res *= BASE;
         if (str[i] <= '9' && str[i] >= '0') {
@@ -101,44 +84,12 @@ constexpr var_t Var(const char *str) {
             res += str[i] - 'A' + 11;
         } else if (str[i] <= 'z' && str[i] >= 'a') {
             res += str[i] - 'a' + 11;
-        } else {
-            return 0;
         }
         ++i;
     }
-    if (str[i] != '\0') return 0;
 
     return res;
 }
-
-/*
-template<var_t Var>
-struct Ref {
-    constexpr static uint64_t &val = Nr<Var>::val;
-
-    constexpr static void set(uint64_t newValue) {
-        val = newValue;
-    }
-};
-
-template<var_t Var, typename Value, typename Expression>
-struct Let {
-    using result = List<Var, Value, Environment>
-
-    constexpr static auto
-
-    run() {
-        Ref<Var>::set(Value::val);
-        return Expression::val;
-    }
-};
-*/
-template<typename T1, typename T2>
-struct Eq {};
-
-template<typename Condition, typename Then, typename Else>
-struct If {};
-
 
 template<typename ValueType>
 class Fibin {
@@ -149,7 +100,7 @@ private:
         return fib(N - 1) + fib(N - 2);
     }
 
-    template<typename T>
+    template<typename Expression, typename Env>
     struct Eval {
     };
 
@@ -157,8 +108,48 @@ private:
     struct Get {
     };
 
-    template<int N>
-    struct Eval<Fib<N>> {
+    template<typename T, typename... Rest>
+    struct SumAux {
+    };
+
+    template<typename T>
+    struct SumAux<T> {
+    };
+
+    template<typename Proc, typename Value>
+    struct Apply {
+    };
+
+    struct EmptyEnv;
+
+    template<typename Lam, typename Env>
+    struct Closure {
+    };
+
+    template<var_t Name, typename Value, typename Env>
+    struct List {
+    };
+
+    template<var_t Name, typename Env>
+    struct Find {
+    };
+
+    template<var_t Name>
+    struct Find<Name, EmptyEnv> {
+    };
+
+    template<var_t Name, typename Value, typename Env>
+    struct Find<Name, List<Name, Value, Env> > {
+        using result = typename Value::result;
+    };
+
+    template<var_t Name, var_t Name2, typename Value2, typename Env>
+    struct Find<Name, List<Name2, Value2, Env> > {
+        using result = typename Find<Name, Env>::result;
+    };
+
+    template<int N, typename Env>
+    struct Get<Fib<N>, Env> {
         static constexpr ValueType value = fib(N);
     };
 
@@ -167,49 +158,35 @@ private:
         using result = Get<Fib<N>, Env>;
     };
 
-    template<typename T1, typename T2>
-    struct Eval<Eq<T1, T2>> {
-        static constexpr bool value = Eval<T1>::value == Eval<T2>::value;
+    template<typename T, typename Env>
+    struct Eval<Lit<T>, Env> {
+        using result = T;
     };
 
-    template<typename T, typename ...Types, typename Env>
-    static ValueType sum() {
-        return Eval<T, Env>::value + sum<Types..., Env>();
-    }
+    template<typename T, typename Env>
+    struct Get<SumAux<T>, Env> {
+        static constexpr ValueType value = Eval<T, Env>::result::value;
+    };
 
-    template<typename T1, typename T2, typename... Rest, typename Env>
-    struct Get<Sum<T1, T2, Rest...>, Env> {
-        static constexpr ValueType value = sum<T1, T2, Rest..., Env>();
+    template<typename T1, typename... Rest, typename Env>
+    struct Get<SumAux<T1, Rest...>, Env> {
+        static constexpr ValueType value =
+                Eval<T1, Env>::result::value + Get<SumAux<Rest...>, Env>::value;
     };
 
     template<typename T1, typename T2, typename... Rest, typename Env>
     struct Eval<Sum<T1, T2, Rest...>, Env> {
-        using result = Get<Sum<T1, T2, Rest...>, Env>;
-    };
-
-    template<int N, typename T, typename Env>
-    static constexpr ValueType inc() {
-        return Get<Fib<N>, Env>::value + Get<T, Env>::value;
-    }
-
-    template<typename T, typename Env>
-    struct Get<Inc1<T>, Env> {
-        static constexpr ValueType value = inc<1, T, Env>();
-    };
-
-    template<typename T, typename Env>
-    struct Get<Inc10<T>, Env> {
-        static constexpr ValueType value = inc<10, T, Env>();
+        using result = Get<SumAux<T1, T2, Rest...>, Env>;
     };
 
     template<typename T, typename Env>
     struct Eval<Inc1<T>, Env> {
-        using result = Get<Inc1<T>, Env>;
+        using result = Get<SumAux<Lit<Fib<1>>, T>, Env>;
     };
 
     template<typename T, typename Env>
     struct Eval<Inc10<T>, Env> {
-        using result = Get<Inc10<T>, Env>;
+        using result = Get<SumAux<Lit<Fib<10>>, T>, Env>;
     };
 
     template<bool flag, typename T1, typename T2>
@@ -221,15 +198,24 @@ private:
         typedef T2 result;
     };
 
+    static constexpr bool eq(ValueType v1, ValueType v2) {
+        return v1 == v2;
+    }
+
     template<typename T1, typename T2, typename Env>
     struct Eval<Eq<T1, T2>, Env> {
         using result = typename If_then_else<Eval<T1, Env>::result::value ==
                                              Eval<T2, Env>::result::value, True, False>::result;
     };
 
-    template<typename T, typename... Rest>
-    struct SumAux {
-        constexpr static ValueType value = SumAux<T>::value + SumAux<Rest...>::value;
+    template<var_t Var, typename Env>
+    struct Eval<Ref<Var>, Env> {
+        using result = typename Find<Var, Env>::result;
+    };
+
+    template<var_t Var, typename Value, typename Expression, typename Env>
+    struct Eval<Let<Var, Value, Expression>, Env> {
+        using result = typename Eval<Expression, List<Var, Value, Env>>::result;
     };
 
     template<typename Then, typename Else, typename Env>
@@ -247,47 +233,28 @@ private:
         using result = typename Eval<If<typename Eval<Condition, Env>::result, Then, Else>, Env>::result;
     };
 
-    template<typename T>
-    struct SumAux<T> {
-        constexpr static ValueType value = Eval<T>::value;
-    };
-
     // TODO
-    template<var_t Var, typename Body>
+    /*template<var_t Var, typename Body>
     struct Eval<Lambda<Var, Body>> {
-        using result = Closure<Lambda<Var, Body>, EmptyEnv>;
+        using result = Closure<Lambda<Var, Body>, Env>;
     };
 
-    /*template<bool flag, typename T1, typename T2>
-    struct If_then_else {
-        typedef T1 Result;
+    template<typename Fun, typename Param>
+    struct Eval<Invoke<Fun, Param>> {
+        using result = Apply<Eval<Fun>::result, Eval<Param>::result>::result
     };
 
-    template<typename T1, typename T2>
-    struct If_then_else<false, T1, T2> {
-        typedef T2 Result;
+    template<var_t Var, typename Body, typename Value>
+    struct Apply<Closure<Lambda<Var, Body>, Env>, Value> {
+        typename Eval<Body, Binding < Name, Value, Env> > ::result
+        typedef result;
     };*/
-    template<bool flag, typename T1, typename T2>
-    struct If_then_else {
-        static constexpr ValueType value = Eval<T1>::value;
-    };
-
-    template<typename T1, typename T2>
-    struct If_then_else<false, T1, T2> {
-        static constexpr ValueType value = Eval<T2>::value;
-    };
-
-    template<typename Condition, typename Then, typename Else>
-    struct Eval<If<Condition, Then, Else>> {
-        static constexpr ValueType value = If_then_else<Eval<Condition>::value, Then, Else>::value;
-    };
-
 
 
 public:
     template<typename Expr, typename X = ValueType, typename std::enable_if_t<std::is_integral<X>::value, int> = 0>
     static constexpr ValueType eval() {
-        return Eval<Expr>::value;
+        return Eval<Expr, EmptyEnv>::result::value;
     }
 
     template<typename Expr, typename X = ValueType, typename std::enable_if_t<!std::is_integral<X>::value, int> = 0>
@@ -297,3 +264,4 @@ public:
 };
 
 #endif //FIBIN_FIBIN_H
+
